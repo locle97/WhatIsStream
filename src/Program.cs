69577f5c18818bec6296b676ab50fa6877c2fb6c
Loc Le <lpl212757@gmail.com>
2024-09-08 04:04:41 +0700

    Implement stream processing from oilwell to gasStation file

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+
+string oilWellPath = "/home/lpl212757/Coding/Research/WhatIsStream/src/oil-well.txt";
+string gasStationPath = "gas-station.txt";
+
+using FileStream oilWell = new FileStream(oilWellPath, FileMode.Open);
+using FileStream gasStation = new FileStream(gasStationPath, FileMode.Create);
+byte[] buffer = new byte[1024];
+while (oilWell.Read(buffer, 0, buffer.Length) > 0)
+{
+    string rawOil = Encoding.UTF8.GetString(buffer);
+    string processedGasoline = rawOil.Replace("Oil", "Gasoline");
+    // Return the processed gasoline data
+    buffer = Encoding.UTF8.GetBytes(processedGasoline);
+    gasStation.Write(buffer, 0, buffer.Length);
+}
